16211babcbcd29230ecebbfafd04fd3cb97940c1
delster1 <delsterone@gmail.com>
2024-03-25 09:59:15 -0500

    fixed directories

---
+++ 3-7-24_cloud_tech/creds.py
@@ -0,0 +1,3 @@
+redis_host = 'redis-11661.c1.asia-northeast1-1.gce.cloud.redislabs.com'
+redis_port = 11661
+redis_password = 'bKez8jCC9Dq0FIWrqKuGLevjcQjBu7M2'
--- 9-7-23_versionControl/main.py
+++
@@ -1,8 +0,0 @@
-def main():
-    print("Welcome to our workshop! ")
-    
-if __name__ == "__main__":
-    main()
-
-
-
