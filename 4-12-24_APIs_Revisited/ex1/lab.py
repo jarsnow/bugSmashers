a425d4e17df5ee4e466c652ad388a14039ec7d3a
delster1 <delsterone@gmail.com>
2024-04-12 16:02:13 -0500

    updated todolist code to be publicly accessible so others can add todos

--- 4-12-24_APIs_Revisited/ex1/lab.py
+++ 4-12-24_APIs_Revisited/ex1/lab.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 import json
+from itertools import count
 
 app = Flask(__name__)
+
+# Counter for task IDs
+id_counter = count(start=1)
 
 # Load tasks from tasks.json
 def load_tasks():
@@ -28,12 +32,19 @@ def index():
 def add_task():
     content = request.form['content']
     tasks = load_tasks()
-    task = {'content': content}
+    task = {'id': next(id_counter), 'content': content}
     tasks.append(task)
     save_tasks(tasks)
     return render_template('index.html', tasks=tasks)
 
-
+# Route to remove a task
+@app.route('/remove', methods=['POST'])
+def remove_task():
+    task_id = int(request.form['task_id'])
+    tasks = load_tasks()
+    tasks = [task for task in tasks if task['id'] != task_id]
+    save_tasks(tasks)
+    return render_template('index.html', tasks=tasks)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
